da7b9d783130d9d315924f2d94336b81dfb8a3ce
caner <66187631+cnrgrl@users.noreply.github.com>
2023-07-27 14:37:58 +0200

    dev to prod (#1)
    * home component created
    
    * redirect set
    
    * bootstrap added
    
    * assets added
    
    * json-server is set
    
    * blog data rendered
    
    * card css prettified
    
    * pagination
    
    * dialog component added
    
    * dialog button options
    
    * blog datas to dialog window
    
    * view comments  in dialog implemented
    
    * reactive forms for update added
    
    * dialog update functionality added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { BaseService } from './services/base.services';
+import { HttpClientModule } from '@angular/common/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BlogDialogComponent } from './home/blog-dialog/blog-dialog.component';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatDividerModule } from '@angular/material/divider';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, HomeComponent, BlogDialogComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatDialogModule,
+    HttpClientModule,
+    MatDividerModule,
+    NgbModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [BaseService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
